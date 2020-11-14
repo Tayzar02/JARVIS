66797cd35c8e6867d33e2151880c08ea05cce084
Tayzar02 <tayhouse0508@gmail.com>
2020-11-14 12:38:17 -0500

    updates to engine. added weather capability

--- code/engine.py
+++ code/engine.py
@@ -1,28 +1,50 @@
 import pyttsx3
 import speech_recognition as sr
+import sys
+import webbrowser
+import pyowm
+import os
+import time
 
-def talk(text):
-	engine = pyttsx3.init()
-	rate = engine.getProperty('rate')
-	engine.setProperty('rate', rate-50)
-	voices = engine.getProperty('voices')
-	engine.setProperty('voice', voices[1].id)
-	engine.say(text)
-	engine.runAndWait()
+os.environ['TZ'] = 'US/Eastern'
+time.tzset()
+time.ctime()
+owm = pyowm.OWM("28ae4a7d1c56086a19ca2b18e2d858de")
+observation = owm.weather_at_place("21911")
+w = observation.get_weather()
+temperature = w.get_temperature('fahrenheit')
+text = ''
+while 'shut down' not in text:
+	print('\033[1;31;40m Jarvis ready, say a command \n')
+	def talk(text):
+		engine = pyttsx3.init()
+		rate = engine.getProperty('rate')
+		engine.setProperty('rate', rate-50)
+		voices = engine.getProperty('voices')
+		engine.setProperty('voice', voices[1].id)
+		engine.say(text)
+		engine.runAndWait()
 
 
-def get_audio():
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		audio = r.listen(source)
-		said = ""
-
-		try:
-			said = r.recognize_google(audio)
-			print(said)
-		except Exception as e:
-			print("Exception: "+ str(e))
-	return said.lower()
-
-text = get_audio()
-print(text)
+	def get_audio():
+		r = sr.Recognizer()
+		with sr.Microphone() as source:
+			audio = r.listen(source)
+			said = ""
+	
+			try:
+				said = r.recognize_google(audio)
+				print(said)
+			except Exception as e:
+				print("Exception: "+ str(e))
+		return said.lower()
+	
+	text = get_audio()
+	print('\033[1;32;40m %s \n' % text)
+	
+	if ('weather' in text):
+		print('yes sir, it is currently ', int(temperature['temp']), 'degrees outside in Port Deposit, Maryland')
+	#elif ()
+	else:
+		continue
+sys.exit()
